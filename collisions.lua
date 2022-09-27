2b53947ca240649dd27b3ccc994401061c3a1265
David Wildman <david.m.wildman@gmail.com>
2022-09-27 14:45:18 +0100

    update collisions.lua

--- collisions.lua
+++ collisions.lua
@@ -1,31 +1,40 @@
-Collisions = {}
-
-function Collisions.beginContact(a, b, collision)
+function BeginContact(a, b, collision)
   for _, boy in ipairs(AllBoyz) do
-    if boy ~= nil and collision ~= nil then
-      boy:beginContact(a, b, collision)
+    if boy ~= nil then
+      if a == boy.fixture or b == boy.fixture then
+        boy:beginContact(a, b, collision)
+      end
     end
   end
 end
 
-function Collisions.endContact(a, b, collision)
+function EndContact(a, b, collision)
   for _, boy in ipairs(AllBoyz) do
-    if boy ~= nil and collision ~= nil then
-      boy:endContact(a, b, collision)
+    if boy ~= nil then
+      if a == boy.fixture or b == boy.fixture then
+        boy:endContact(a, b, collision)
+      end
     end
   end
 end
 
-function Collisions.preSolve(a, b, collision)
+function PreSolve(a, b, collision)
   print(a, b, collision)
 
 end
 
-function Collisions.postSolve(a, b, collision, normalimpulse, tangentimpulse)
+function PostSolve(a, b, collision, normalimpulse, tangentimpulse)
   print(a, b, collision, normalimpulse, tangentimpulse)
-
 end
 
-return Collisions
+function IsOnTop(object, fixture_a, fixture_b, normal_y)
+  if fixture_a == object then
+    return normal_y > 0
+  elseif fixture_b == object then
+    return normal_y < 0
+  end
+end
+
+
 
 -- https://love2d.org/wiki/Tutorial:PhysicsCollisionCallbacks -- this is what I need to add sounds on collision events
