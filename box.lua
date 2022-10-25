86762fba29cbb87d49533afeb9d9b3340c26a550
David Wildman <david.m.wildman@gmail.com>
2022-10-25 01:16:25 +0100

    update box color

--- box.lua
+++ box.lua
@@ -1,4 +1,4 @@
 Box = { size = ENVIRONMENT.screen_width * 0.1, color = { 0.1, 0.3, 0.5 }, text = love.graphics.newText(love.graphics.newFont("assets/choreboyz.ttf", 20), "CHOREBOYZ") }
 Box.update = function (dt)
-  if love.keyboard.isDown("x") then Box.color = { 0.5, 0.3, 0.1 } else Box.color = { 0.1, 0.3, 0.5 } end
+  if love.keyboard.isDown("x") then Box.color = { 0.1, 0.3, 0.5 } else Box.color = { 0.5, 0.3, 0.1 } end
 end
