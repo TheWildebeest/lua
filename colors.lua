b61851cc02b2afe96d2198d8c93927b51957eb28
David Wildman <david.m.wildman@gmail.com>
2022-12-22 00:33:29 +0000

    add colors

--- colors.lua
+++ colors.lua
@@ -1,10 +1,11 @@
 return {
+  black = { 0, 0, 0 },
+  black_alpha = { 0, 0, 0, 0.75 },
+  white = { 1, 1, 1 },
   brown_light =   {0.57, 0.55, 0.44},
   brown_dark = { 0.26, 0.22, 0.19},
   yellow = {0.76, 0.77, 0.55},
   green = { 0.82, 0.94, 0.71 },
-  black = { 0, 0, 0 },
-  black_alpha = { 0, 0, 0, 0.75 },
   bulb_off = { 0.8, 0.8, 1, 0.2 },
   bulb_on = { 0.99, 0.99, 0.99, 0.6}
 }
